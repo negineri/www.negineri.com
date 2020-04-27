92cd1c3317463df5c1480290a46aed8e101c821b
negineri <harusoin@gmail.com>
2020-04-27 04:48:38 +0000

    full typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Typescript Starter`,
+    authors: [
+      { name: 'Tori', slug: 'tori' },
+      { name: 'Neko', slug: 'neko' },
+      { name: 'Inu', slug: 'inu' }
+    ]
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-graphql-codegen',
+      options: {
+        fileName: `types/graphql-types.d.ts`
+      }
+    },
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,26 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
+import { Link, graphql } from 'gatsby'
+import { IndexQueryQuery } from "../../types/graphql-types"
 
-// Please note that you can use https://github.com/dotansimha/graphql-code-generator
-// to generate all types from graphQL schema
-interface IndexPageProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string
-      }
-    }
-  }
+// ______________________________________________________
+//
+type Props = {
+  data: IndexQueryQuery
 }
+// ______________________________________________________
+//
 
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-  public render() {
-    return (
-      <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </div>
-    )
-  }
-}
+const Component: React.FC<Props> = ({ data }) => (
+  <div>
+    <h1>Hi people</h1>
+    <p>
+      Welcome to your new{" "}
+      <strong>{data.site?.siteMetadata?.title}</strong> site.
+    </p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -41,3 +31,4 @@ export const pageQuery = graphql`
     }
   }
 `
+export default Component
